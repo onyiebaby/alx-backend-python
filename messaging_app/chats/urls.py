f3a215375c80d5803674a9f2d306ed3c34ff2427
Onyiebaby <onyinyechimatthew06@gmail.com>
2025-10-05 20:02:01 +0100

    Add urls to messaging app

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -6,9 +6,10 @@ router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 
 conversations_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
-conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages'
 
 urlpatterns = [
     path('', include(router.urls)),
     path('', include(conversations_router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
