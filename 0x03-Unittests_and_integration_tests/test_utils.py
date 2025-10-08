ceab87148075575c1c414df7f9f0c3d424e940d1
Onyiebaby <onyinyechimatthew06@gmail.com>
2025-10-08 16:01:13 +0100

    Add utils.py for unittesting

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python3
+"""Unit tests for utils.py"""
+
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import get_json
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Test class for access_nested_map function."""
+    """Test case for the get_json function."""
 
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
-        "Test access_nested_map returns expected results"
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Test that utils.get_json returns expected results"""
+        mock_response = Mock ()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+        
+        result = get_json(test_url)
 
-    @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
-    ])    
-    def test_access_nested_map_exception(self, nested_map, path):
-        """Test that KeyError is raised for invalid paths."""
-        with self.assertRaises(KeyError) as error:
-            access_nested_map(nested_map, path)
-        self.assertAlmostEqual(str(error.exception), f"'{path[-1]}'")    
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
+
+
+     
     
 
         
