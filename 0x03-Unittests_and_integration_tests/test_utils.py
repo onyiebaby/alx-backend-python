2038e942fdba26e92f8c98d3a1d38402d73352e9
Onyiebaby <onyinyechimatthew06@gmail.com>
2025-10-08 16:36:48 +0100

    Add utils.py for unittesting

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,27 +3,28 @@
 
 import unittest
 from unittest.mock import patch, Mock
-from parameterized import parameterized
 from utils import get_json
 
-class TestAccessNestedMap(unittest.TestCase):
-    """Test case for the get_json function."""
+class TestGetJson(unittest.TestCase):
+    """Test case for utils.get_json function."""
 
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False}),
-    ])
-    @patch('utils.requests.get')
-    def test_get_json(self, test_url, test_payload, mock_get):
-        """Test that utils.get_json returns expected results"""
-        mock_response = Mock ()
-        mock_response.json.return_value = test_payload
-        mock_get.return_value = mock_response
+    def test_get_json(self):
+        """Test that get_json returns expected result with mocked requests.get"""
+        test_cases = [
+            ("http://example.com", {"payload": True})
+            ("http://holberton.io", {"payload": False}),
+        ]
         
-        result = get_json(test_url)
+        for test_url, test_payload in test_cases:
+            with patch('utils.requests.get') as mock_get:
+                mock_response = Mock ()
+                mock_response.json.return_value = test_payload
+                mock_get.return_value = mock_response
+        
+                result = get_json(test_url)
 
-        mock_get.assert_called_once_with(test_url)
-        self.assertEqual(result, test_payload)
+                mock_get.assert_called_once_with(test_url)
+                self.assertEqual(result, test_payload)
 
 
      
