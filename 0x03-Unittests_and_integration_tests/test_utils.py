7c54bdb8262c07f81c11a5d68baa713389f0333d
Onyiebaby <onyinyechimatthew06@gmail.com>
2025-10-08 13:17:41 +0100

    Add utils.py for unittesting

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@ from parameterized import parameterized
 from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Unit tests for utils.access_nested_map"""
+    """Test class for access_nested_map function."""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -14,6 +14,16 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         "Test access_nested_map returns expected results"
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])    
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test that KeyError is raised for invalid paths."""
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(nested_map, path)
+        self.assertAlmostEqual(str(error.exception), f"'{path[-1]}'")    
     
 
         
