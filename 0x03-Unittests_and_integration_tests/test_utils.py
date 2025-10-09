c131b48e721c6c6d7d5855b2d7e67cd98518cc52
Onyiebaby <onyinyechimatthew06@gmail.com>
2025-10-09 03:08:01 +0100

    Add utils.py for unittesting

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -29,6 +29,7 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
         self.assertEqual(str(err.expected), repr(path[-1]))
 
+
 class TestGetJson(unittest.TestCase):
     """Test case for get_json function."""
 
@@ -61,8 +62,10 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
-                obj = TestClass()
-                self.assertEqual(obj.a_property, 42)
-                self.assertEqual(obj.a_property, 42)
-                mock_method.assert_called_once()
+        with patch.object
+        (TestClass, "a_method", return_value=42
+         ) as mock_method:
+             obj = TestClass()
+             self.assertEqual(obj.a_property, 42)
+             self.assertEqual(obj.a_property, 42)
+             mock_method.assert_called_once()
