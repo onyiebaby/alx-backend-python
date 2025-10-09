b445adbf20fe0ffc684aa3b2ef532a507c864bf8
Onyiebaby <onyinyechimatthew06@gmail.com>
2025-10-09 03:14:53 +0100

    Add utils.py for unittesting

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -66,6 +66,7 @@ class TestMemoize(unittest.TestCase):
             TestClass, "a_method", return_value=42
          ) as mock_method:
              obj = TestClass()
+             
              self.assertEqual(obj.a_property, 42)
              self.assertEqual(obj.a_property, 42)
              mock_method.assert_called_once()
