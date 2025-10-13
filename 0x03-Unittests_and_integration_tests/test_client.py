7cd595a6a9095e8bc65b80540dd19d259eb26e39
Onyiebaby <onyinyechimatthew06@gmail.com>
2025-10-13 20:07:17 +0100

    Add clients.py and parameterized to unittest

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import unittest
 from unittest.mock import patch, PropertyMock
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
 from client import GithubOrgClient
 from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
 
